1bacf226887a9e0a01ed411e97a78cd2e8d414fe
João Lucas <joaolucas7371@gmail.com>
2021-11-23 13:16:12 -0300

    first commit

---
+++ Desafio 3/index.js
@@ -0,0 +1,9 @@
+let mensagemInicial = confirm ('Seja bem vindo(a), aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?')
+    console.log(mensagemInicial)
+
+if(mensagemInicial == true){
+    alert('Ótimo! Nós temos as melhores camas de toda região')
+}else{
+    alert('Que pena! Você parecia ser uma pessoa mais legal')
+}
+
---
+++ Desafio 6/index.js
@@ -0,0 +1,23 @@
+function somar(numero1, numero2){
+    alert('Os números para soma são 1 e 7')
+    console.log('A soma dos dois números é: ' + (1 + 7))
+}
+somar()
+
+function subtrair(numero1, numero2){
+    alert('Os números para subtração são 10 e 5')
+    console.log('A subtração dos dois números é: ' + (10-5))
+}
+subtrair()
+
+function multiplicacao(numero1, numero2){
+    alert('Os números para multiplicação são 2 e 4')
+    console.log('A multiplicação dos dois números é: ' + (2 * 4))
+}
+multiplicacao()
+
+function divisao(numero1, numero2){
+    alert('Os números para divisão são 12 e 2')
+    console.log("A divisão dos dois números é: " + (12/2))
+}
+divisao()
