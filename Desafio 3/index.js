0373dd1eab0e93974371ff70d25332afbc48aec5
João Lucas <joaolucas7371@gmail.com>
2021-11-30 12:12:51 -0300

    mudanca-variavel-condicao

--- Desafio 3/index.js
+++ Desafio 3/index.js
@@ -1,9 +1,9 @@
-let mensagemInicial = confirm ('Seja bem vindo(a), aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?')
-    console.log(mensagemInicial)
+const seHospedou = confirm('Seja bem vindo(a), aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?')
+console.log(seHospedou)
 
-if(mensagemInicial == true){
+if (seHospedou) {
     alert('Ótimo! Nós temos as melhores camas de toda região')
-}else{
+} else {
     alert('Que pena! Você parecia ser uma pessoa mais legal')
 }
 
