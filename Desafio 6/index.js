3db191dae7a7d79d155990fc10e49d75c3cdd69d
João Lucas <joaolucas7371@gmail.com>
2021-11-30 13:12:08 -0300

    mudanca-return

--- Desafio 6/index.js
+++ Desafio 6/index.js
@@ -1,23 +1,19 @@
-function somar(numero1, numero2){
-    alert('Os números para soma são 1 e 7')
-    console.log('A soma dos dois números é: ' + (1 + 7))
+function somar(numero1, numero2) {
+    return numero1 + numero2
 }
-somar()
+console.log('O resultado da soma é:', somar(1, 28))
 
-function subtrair(numero1, numero2){
-    alert('Os números para subtração são 10 e 5')
-    console.log('A subtração dos dois números é: ' + (10-5))
+function subtrair(numero1, numero2) {
+    return numero1 - numero2
 }
-subtrair()
+console.log('A subtração dos dois números é:',subtrair(10, 5))
 
-function multiplicacao(numero1, numero2){
-    alert('Os números para multiplicação são 2 e 4')
-    console.log('A multiplicação dos dois números é: ' + (2 * 4))
+function multiplicar(numero1, numero2) {
+    return numero1 * numero2   
 }
-multiplicacao()
+console.log('A multiplicação dos dois números é:', multiplicar(2, 4))
 
-function divisao(numero1, numero2){
-    alert('Os números para divisão são 12 e 2')
-    console.log("A divisão dos dois números é: " + (12/2))
+function dividir(numero1, numero2) {
+    return numero1 / numero2
 }
-divisao()
+console.log('A divisão dos dois números é:', dividir(12, 2))
